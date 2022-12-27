9f6a6b4a3017093127108ff9efbec2dfc1953dcc
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2022-12-27 12:56:42 -0800

    Debuging-login feature

---
+++ Login/login.js
@@ -0,0 +1,13 @@
+function myFunction(){
+    //var user = document.getElementById("usr").value;
+    //var password = document.getElementsByTagName("pwd").value;
+    if(1)
+    {
+        alert("Login Successfully");
+        window.location.href = "../index.html";
+    }
+    else {
+        alert("Invalid information");
+        return ;
+    }
+}
