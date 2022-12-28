3f550cb4325d43421632671e23cf24ae651c14ae
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2022-12-27 17:27:09 -0800

    debuging

--- Login/login.js
+++ Login/login.js
@@ -1,10 +1,11 @@
 function myFunction(){
     //var user = document.getElementById("usr").value;
     //var password = document.getElementsByTagName("pwd").value;
+    //let usr = document.getElementByid("uname").value;
     if(1)
     {
         alert("Login Successfully");
-        window.location.href = "../index.html";
+        setTimeout(() =>{window. location.replace ("../index.html");});
     }
     else {
         alert("Invalid information");
